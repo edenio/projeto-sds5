6ab168a83dbee379d39b72ad2f3de62ba8b82def
Edenio Oliveira <edenio_cdi@yahoo.com.br>
2021-11-02 14:45:14 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 function App() {
   return (
     <>
-      <NavBar/>
-      <DataTable/>
-      <Footer/>
-      
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+        </div>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Grafico de Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+
     </>
   );
 }
